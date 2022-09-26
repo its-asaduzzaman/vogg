2a953384144c2d563025c6526f609cd7a68e0d4d
Asaduzzaman_mac <samrat.asad@gmail.com>
2022-09-26 21:47:07 +0600

    added dot indicator for slid the widget

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,3 +1,4 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:vogg/utils/colors.dart';
 import 'package:vogg/widgets/big_text.dart';
@@ -28,21 +29,36 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 320,
-      child: PageView.builder(
-          controller: pageController,
-          itemCount: 5,
-          itemBuilder: (context, position) {
-            return _buildPAgeItem(position);
-          }),
-    );
+  void dispose() {
+    pageController.dispose();
   }
 
   @override
-  void dispose() {
-    pageController.dispose();
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 320,
+          child: PageView.builder(
+              controller: pageController,
+              itemCount: 5,
+              itemBuilder: (context, position) {
+                return _buildPAgeItem(position);
+              }),
+        ),
+        DotsIndicator(
+          dotsCount: 5,
+          position: _currPageValue,
+          decorator: DotsDecorator(
+            activeColor: AppColors.mainColor,
+            size: const Size.square(9.0),
+            activeSize: const Size(18.0, 9.0),
+            activeShape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0)),
+          ),
+        ),
+      ],
+    );
   }
 
   Widget _buildPAgeItem(int index) {
