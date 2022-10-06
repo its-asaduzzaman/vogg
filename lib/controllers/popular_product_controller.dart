bc59245df7f8984c7dc257b6e588bbc4a5921599
Asaduzzaman_mac <samrat.asad@gmail.com>
2022-10-06 15:14:21 +0600

    repository and controller

---
+++ lib/controllers/popular_product_controller.dart
@@ -0,0 +1,18 @@
+import 'package:get/get.dart';
+import 'package:vogg/data/repository/popular_product_repo.dart';
+import '../data/repository/popular_product_repo.dart';
+
+class PopularProductController extends GetxController {
+  final PopularProductRepo popularProductRepo;
+  PopularProductController({required this.popularProductRepo});
+  List<dynamic> _popularProductList = [];
+
+  Future<void> getPopularProductList() async {
+    Response response = await popularProductRepo.getPopularProductList();
+    if (response.statusCode == 200) {
+      _popularProductList = [];
+      _popularProductList.addAll();
+      update();
+    } else {}
+  }
+}
