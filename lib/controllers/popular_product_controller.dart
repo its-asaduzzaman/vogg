f9f928a17548c7c2af8d51f1143432af04b0cbae
Asaduzzaman_mac <samrat.asad@gmail.com>
2022-10-18 18:22:56 +0600

    load repositories in a function

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -6,12 +6,13 @@ class PopularProductController extends GetxController {
   final PopularProductRepo popularProductRepo;
   PopularProductController({required this.popularProductRepo});
   List<dynamic> _popularProductList = [];
+  List<dynamic> get popularProductList => _popularProductList;
 
   Future<void> getPopularProductList() async {
     Response response = await popularProductRepo.getPopularProductList();
     if (response.statusCode == 200) {
       _popularProductList = [];
-      _popularProductList.addAll();
+      // _popularProductList.addAll();
       update();
     } else {}
   }
--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,1 +1,16 @@
-Future<void> init() async {}
+import 'package:get/get.dart';
+import 'package:vogg/data/api/api_client.dart';
+
+import '../controllers/popular_product_controller.dart';
+import '../data/repository/popular_product_repo.dart';
+
+Future<void> init() async {
+  //api clint
+  Get.lazyPut(() => ApiClient(appBaseUrl: "https://www.dbestech.com"));
+
+  //repos
+  Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
+
+  //Controllers
+  Get.lazyPut(() => PopularProductController(popularProductRepo: Get.find()));
+}
